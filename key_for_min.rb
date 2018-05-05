820aa87b576f7b8e0f07fc717789bc6e23e4b1f4
Eric Razor <eric.razorjr@gmail.com>
2018-05-05 20:16:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   val = nil    
   key = nil 
   name_hash.each do |key1,value|
-      if val == nil || value < val
+      if val == nil and value < val
         val = value
         key = key1
       end
